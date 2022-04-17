14a8741f1bb50a6ddb534029712562763132081b
Vlad <vevsyukov@webcodin.com>
2022-04-17 11:39:05 +0300

    first

--- json_parsing.py
+++ json_parsing.py
@@ -2,4 +2,9 @@ import json
 
 string_as_json_format ='{"answer":"Hello, User"}'
 obj=json.loads(string_as_json_format)
-print(obj['answer'])
+
+key = "answer3"
+if key in obj:
+    print(obj[key])
+else:
+    print(f"Ключа {key} в json нет!")
